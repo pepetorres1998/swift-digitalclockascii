525445b00d8fab26b015a8171c85dd6f01fd91b1
Jose Antonio Torres Garibay <pepe_torres1998@hotmail.com>
2025-01-07 08:38:13 -0600

    Initial Commit

---
+++ AsciiDigitalClock/AsciiDigitalClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsciiDigitalClockApp.swift
+//  AsciiDigitalClock
+//
+//  Created by Jose Anonio Torres Garibay on 07/01/25.
+//
+
+import SwiftUI
+
+@main
+struct AsciiDigitalClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsciiDigitalClockTests/AsciiDigitalClockTests.swift
@@ -0,0 +1,17 @@
+//
+//  AsciiDigitalClockTests.swift
+//  AsciiDigitalClockTests
+//
+//  Created by Jose Anonio Torres Garibay on 07/01/25.
+//
+
+import Testing
+@testable import AsciiDigitalClock
+
+struct AsciiDigitalClockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
